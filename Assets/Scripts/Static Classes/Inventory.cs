80a687f97ae242d7961f53927adf7f3604ec0e1e
Anton Lindroth <ntoonio@gmail.com>
2022-11-04 21:51:30 +0100

    Added inventory script

--- Assets/Scripts/Static Classes/Inventory.cs
+++ Assets/Scripts/Static Classes/Inventory.cs
@@ -1,17 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Inventory : MonoBehaviour {
 
     public static Inventory instance;
+	public int maxHealth;
 
     private int wood = 0;
     private int fluff = 0;
     private int candy = 0;
 
+	private int health;
+
     private void Start() {
         instance = this;
+		health = maxHealth;
     }
+
+	// Health functions
+
+	public void addHealth(int newHealth) {
+		health = Math.Min(health + newHealth, maxHealth);
+	}
+
+	public void removeHealth(int damage) {
+		health = Math.Max(health - damage, 0);
+	}
+
+	public void setHealth(int newHealth) {
+		health = newHealth; // Allowing overriding max health
+	}
+
+	public bool isDead() {
+		return health == 0;
+	}
+
+	// Wood functions
+	public bool hasWood(int amount) {
+		return wood >= amount;
+	}
+
+	public void removeWood(int amount) {
+		wood -= amount;
+	}
+
+	public void addWood(int amount) {
+		wood += amount;
+	}
+
+	public void setWood(int value) {
+		wood = value;
+	}
+
+
+	// Fluff functions
+	public bool hasFluff(int amount) {
+		return fluff >= amount;
+	}
+
+	public void removeFluff(int amount) {
+		fluff -= amount;
+	}
+
+	public void addFluff(int amount) {
+		fluff += amount;
+	}
+
+	public void setFluff(int value) {
+		fluff = value;
+	}
+
+
+	// Candy functions
+	public bool hasCandy(int amount) {
+		return candy >= amount;
+	}
+
+	public void removeCandy(int amount) {
+		candy -= amount;
+	}
+
+	public void addCandy(int amount) {
+		candy += amount;
+	}
+
+	public void setCandy(int value) {
+		candy = value;
+	}
 
 }
