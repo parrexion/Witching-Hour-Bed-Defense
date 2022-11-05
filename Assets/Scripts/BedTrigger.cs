889054fbd11e6bc39ccb4f406b2e4a7db3088fd8
Seyloj <Hannes20107@gmail.com>
2022-11-05 18:14:41 +0100

    Fixed anim

--- Assets/Scripts/BedTrigger.cs
+++ Assets/Scripts/BedTrigger.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class BedTrigger : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Triggered by " + other);
         if(other.CompareTag("Enemy")) {
             float damage = other.GetComponent<EnemyHealth>().enemy.damage;
             Inventory.instance.removeHealth(damage);
