a721528f6a610e6144943afb85bc1bae84827cee
Seyloj <Hannes20107@gmail.com>
2022-11-05 23:09:53 +0100

    A

--- Assets/Scripts/BedTrigger.cs
+++ Assets/Scripts/BedTrigger.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BedTrigger : MonoBehaviour {
+
+    public List<string> damageMessages = new List<string>();
+
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Enemy")) {
             Enemy enemy = other.GetComponent<Enemy>();
             Inventory.instance.removeHealth(enemy.GetDamage());
             enemy.ReachedGoal();
-            ChatBubble.instance.displayMessage("Nice tower placement Einstein...");
+            ChatBubble.instance.displayMessage(damageMessages[Random.Range(0, damageMessages.Count)]);
         }
     }
 
